dd809cfb4146d5ae9646b0a8343b4787fec70d7e
PatryMot <brightmate.pm@outlook.com>
2024-04-24 18:02:22 +0900

    test: loading logic with suspense

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,60 @@ import { AppProvider } from "contexts";
 import GlobalStyle from "globalStyles";
 import Loading from "pages/loading";
 import Router from "pages/router";
-// import { Suspense } from "react";
+import { Suspense } from "react";
 import { useEffect, useState } from "react";
+
+let fullfilled = false;
+let promise: Promise<void> | null = null;
+
+const useTimeout = (ms: number) => {
+  if (!fullfilled) {
+    throw (promise ||= new Promise((res: any) => {
+      setTimeout(() => {
+        fullfilled = true;
+        res();
+      }, ms);
+    }));
+  }
+};
+
+const Main = () => {
+  useTimeout(500);
+  return <div>Loaded</div>;
+};
 
 function App() {
   const [state, setState] = useState<string>("loading");
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setState("start");
-    }, 3000);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setState("start");
+  //   }, 3000);
 
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
+  //   return () => {
+  //     clearTimeout(timer);
+  //   };
+  // }, []);
 
-  if (state === "loading") {
-    return <>
-      <GlobalStyle />
-      <Loading />
-    </>
-  }
+  // if (state === "loading") {
+  //   return (
+  //     <>
+  //       <GlobalStyle />
+  //       <Loading />
+  //     </>
+  //   );
+  // }
 
   return (
     <AppProvider>
-      {/* <Suspense fallback={<Loading />}> */}
-      <GlobalStyle />
-      <Header />
-      <Menu />
-      <Router />
-      <Footer />
-      {/* </Suspense> */}
+      <Suspense fallback={<Loading />}>
+        <Main />
+        {/* <GlobalStyle />
+        <Header />
+        <Menu />
+        <Router />
+        <Footer /> */}
+      </Suspense>
     </AppProvider>
   );
 }
